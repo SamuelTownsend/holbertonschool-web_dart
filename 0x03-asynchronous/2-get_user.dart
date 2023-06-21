758861bceb01e186dce7704469134f2f43bd7599
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-06-20 22:35:57 -0500

    adding dart code

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,16 @@
+Future<void> getUser() async {
+	try {
+		print(await fetchUser());
+	} catch (error) {
+		print("error caught: $error");
+	}
+}
+
+Future<String> fetchUser() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
+    
